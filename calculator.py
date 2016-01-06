d6e1e7194574436f5bd95ae093010b88c6d24f27
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 13:02:33 -0800

    Calc 2 first Commit

---
+++ calculator.py
@@ -0,0 +1,43 @@
+"""
+calculator.py
+
+Using our arithmetic.py file from Exercise02, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+
+# Your code goes here
+while True:
+    "input arithmetic problem"
+    #intify raw input
+    user_input = raw_input("")
+    user_input = user_input.split(" ")
+    if len(user_input) > 1:
+        user_input[1] = int(user_input[1])
+        if len(user_input)==3:
+            user_input[2] = int(user_input[2])
+
+    if user_input[0] =="+":
+        print add(user_input[1],user_input[2])
+    elif user_input[0] =="-":
+        print subtract(user_input[1],user_input[2])
+    elif user_input[0] == "*":
+        print multiply(user_input[1], user_input[2])
+    elif user_input[0] == "/":
+        print divide(user_input[1], user_input[2])
+    elif user_input[0] == "square":
+        print square(user_input[1])
+    elif user_input[0] == "cube":
+        print cube(user_input[1])     
+    elif user_input[0] == "pow":
+        print power(user_input[1], user_input[2])
+    elif user_input[0] == "mod":
+        print mod(user_input[1], user_input[2])
+    elif user_input[0] =="q"   :
+        print "Bye!"
+        break 
+    else: 
+        print "broken"
+        break
